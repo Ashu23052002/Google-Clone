7a082b439d99a0e95c0d524937d2e9291a759159
Ashu23052002 <93607640+Ashu23052002@users.noreply.github.com>
2023-12-12 01:33:25 +0530

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -89,7 +89,7 @@ const Main: React.FC = () => {
           className="text-sm bg-slate-100 py-2 px-4 rounded hover:border"
           onClick={() => router.push("http://www.google.com/doodles")}
         >
-          I'm Feeling Lucky
+          I&apos;m Feeling Lucky
           
         </button>
       </div>
